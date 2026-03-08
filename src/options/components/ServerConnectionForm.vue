193928e9f989787c3e5d71b6893d78541ae87823
mic <m@micz.it>
2026-03-08 21:03:39 +0100

    show api key button added

--- src/options/components/ServerConnectionForm.vue
+++ src/options/components/ServerConnectionForm.vue
@@ -1,8 +1,10 @@
 <script setup>
+import { ref } from 'vue'
 import { useConnectionSettingsStore } from '../stores/connection-settings.store.js'
 
 const store = useConnectionSettingsStore()
 const i18n = browser.i18n.getMessage.bind(browser.i18n)
+const showToken = ref(false)
 </script>
 
 <template>
@@ -19,13 +21,23 @@ const i18n = browser.i18n.getMessage.bind(browser.i18n)
     </div>
     <div class="field">
       <label for="pat">{{ i18n('labelPat') }}</label>
-      <input
-        id="pat"
-        v-model="store.apiToken"
-        type="password"
-        :placeholder="i18n('placeholderPat')"
-        :disabled="store.loading"
-      />
+      <div class="input-wrapper">
+        <input
+          id="pat"
+          v-model="store.apiToken"
+          :type="showToken ? 'text' : 'password'"
+          :placeholder="i18n('placeholderPat')"
+          :disabled="store.loading"
+        />
+        <button
+          type="button"
+          class="toggle-btn"
+          :disabled="store.loading"
+          @click="showToken = !showToken"
+        >
+          <img src="/icons/pwd-hide.png" alt="" />
+        </button>
+      </div>
     </div>
   </div>
 </template>
@@ -67,5 +79,51 @@ const i18n = browser.i18n.getMessage.bind(browser.i18n)
 .field input:disabled {
   opacity: 0.6;
   cursor: not-allowed;
+}
+
+.input-wrapper {
+  position: relative;
+  display: flex;
+  align-items: center;
+}
+
+.input-wrapper input {
+  flex: 1;
+  padding-right: 36px;
+}
+
+.toggle-btn {
+  position: absolute;
+  right: 0;
+  height: 100%;
+  width: 36px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: none;
+  border: none;
+  cursor: pointer;
+  padding: 0;
+  opacity: 0.6;
+}
+
+.toggle-btn:hover {
+  opacity: 1;
+}
+
+.toggle-btn:disabled {
+  opacity: 0.3;
+  cursor: not-allowed;
+}
+
+.toggle-btn img {
+  width: 18px;
+  height: 18px;
+}
+
+@media (prefers-color-scheme: dark) {
+  .toggle-btn img {
+    filter: invert(1);
+  }
 }
 </style>

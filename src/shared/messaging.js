25acacc54ddcfa6a1dda36e814f0479ceb51f18b
mic <m@micz.it>
2026-03-13 21:07:45 +0100

    license header added to js files

--- src/shared/messaging.js
+++ src/shared/messaging.js
@@ -1,3 +1,21 @@
+/*
+ *  ThunderJira [https://micz.it/thunderbird-addon-thunderjira/]
+ *  Copyright (C) 2026 Mic (m@micz.it)
+
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 // Message type constants
 export const JIRA_GET_PROJECTS    = 'JIRA_GET_PROJECTS'
 export const JIRA_GET_ISSUE_TYPES = 'JIRA_GET_ISSUE_TYPES'
